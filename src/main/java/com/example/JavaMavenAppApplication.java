737ef25cfde5f814eefdc8d08dd75e5ec74533f8
aldi2008 <aldiperdiyatna79@gmail.com>
2025-10-22 16:35:27 +0700

    Add Spring Boot main Java file

---
+++ src/main/java/com/example/JavaMavenAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamavenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMavenAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMavenAppApplication.class, args);
+    }
+
+}
